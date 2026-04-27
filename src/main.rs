259e83d145b9e81021bfce0dca8172976dd8e461
Taras <tarasst@gmail.com>
2026-04-27 19:32:11 +0200

    use buf reader

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use anyhow::{Context, Result, anyhow};
 use std::fs;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 use std::path::PathBuf;
 use std::str::FromStr;
 use vsvg::{Color, DocumentTrait, LayerTrait, PathTrait};
@@ -37,7 +40,42 @@ fn read_points<'a>(input: &'a str) -> impl 'a + Iterator<Item = (f64, f64)> {
     extract_points(input).map(|(x, y)| (x * SCALE, (4400. - y) * SCALE))
 }
 
-fn main() {
+fn elaborate(
+    buf: &str,
+    layer: &mut vsvg::Layer,
+    current: &mut Option<(f64, f64)>,
+    color: &mut Color,
+) {
+    for cmd in buf.split(';') {
+        if cmd.starts_with("SP") {
+            let num = usize::from_str(&cmd[2..]).unwrap();
+            *color = COLORS[num];
+        }
+
+        if cmd.starts_with("PU") {
+            if cmd.len() == 2 {
+                continue;
+            }
+            let sub = &cmd[2..];
+
+            if let Some(point) = read_points(sub).last() {
+                current.replace(point);
+            }
+        }
+
+        if cmd.starts_with("PD") {
+            let sub = &cmd[2..];
+
+            let all_points = current.take().into_iter().chain(read_points(sub));
+            let mut poly = vsvg::Path::from_points(all_points);
+            poly.metadata_mut().color = *color;
+            poly.metadata_mut().stroke_width = 1.0;
+            layer.paths.push(poly);
+        }
+    }
+}
+
+fn main() -> Result<()> {
     let args = Args::parse();
 
     let output = args
@@ -50,43 +88,21 @@ fn main() {
     let mut layer = vsvg::Layer::default();
     layer.metadata_mut().name = Some("Layer 2".to_string());
 
-    let mut hpgl = fs::read_to_string(&args.input).expect("Should have been able to read the file");
+    let mut reader = BufReader::with_capacity(4096 * 16, File::open(&args.input)?);
 
-    hpgl.retain(|c| c != '\n');
+    let mut chunks = reader.split(b';');
 
-    let mut start: Option<(f64, f64)> = None;
-
+    let mut current_point: Option<(f64, f64)> = None;
     let mut color = COLORS[0];
 
-    for cmd in hpgl.split(';') {
-        if cmd.starts_with("SP") {
-            let num = usize::from_str(&cmd[2..]).unwrap();
-            color = COLORS[num];
+    while let Some(Ok(chunk)) = chunks.next() {
+        if chunk.starts_with(b"\r") {
+            break;
         }
-
-        if cmd.starts_with("PU") {
-            if cmd.len() == 2 {
-                continue;
-            }
-            let sub = &cmd[2..];
-
-            if let Some(point) = read_points(sub).last() {
-                start.replace(point);
-            }
-        }
-
-        if cmd.starts_with("PD") {
-            let sub = &cmd[2..];
-
-            let all_points = start.into_iter().chain(read_points(sub));
-            let mut poly = vsvg::Path::from_points(all_points);
-            poly.metadata_mut().color = color;
-            poly.metadata_mut().stroke_width = 1.0;
-            layer.paths.push(poly);
-        }
+        let buf = String::from_utf8(chunk)?;
+        elaborate(&buf, &mut layer, &mut current_point, &mut color);
     }
 
-    // save to SVG
     doc.layers_mut().insert(2, layer);
-    doc.to_svg_file(&output).unwrap();
+    doc.to_svg_file(&output).context("Failed to write SVG file")
 }
